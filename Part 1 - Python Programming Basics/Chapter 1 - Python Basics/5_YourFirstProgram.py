b47e038df5aceca3819208410b01b5c09afea150
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-21 14:28:08 -0600

    First Program from Chapter 1
    Every note about the 'Dissecting the program' is in Notion.
    Extra Credits are so cool (finally)!

---
+++ Part 1 - Python Programming Basics/Chapter 1 - Python Basics/5_YourFirstProgram.py
@@ -0,0 +1,28 @@
+
+# This program says hellos and asks for my name
+
+print("\nHello w!")
+
+print("Whats is your name?") # ask for their name
+myName = input()
+
+print('It is good to meet you, dawg, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+
+print('What is your age?') #
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year')
+
+# //////////////////////////////////////////
+
+"""
+    DISSECTING THE PROGRAM
+
+    # this is a comment
+    
+    -> Sometimes, programmers will put a # in front of a line of code to temporarily remove it while testing a program. This is called commenting out
+
+    // ALL NOTES IN NOTION :)
+
+"""
