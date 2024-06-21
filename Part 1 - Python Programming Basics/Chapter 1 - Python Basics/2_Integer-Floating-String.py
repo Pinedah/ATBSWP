d80f075c1d00bc1fab1fbd4122c7e328cc270a77
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-21 12:47:05 -0600

    Integer, Floating, String from Chapter 1

---
+++ Part 1 - Python Programming Basics/Chapter 1 - Python Basics/2_Integer-Floating-String.py
@@ -0,0 +1,15 @@
+
+# . A data type is a category for values, and every value belongs to exactly one data type
+
+# INTEGERS:         -2, -1, 0, 1, 2, ... belong to Z
+# FLOATING-POINT:    -1.22, -1.0, 0.0, 0.1, 11.11, ... belong to Q
+# STRINGS:          'a' , 'aa' , 'aaa' , 'Hello!' , '11 cats xd'
+
+# -> you can have a string with no characters in it: '' (it's called a blank string)
+
+print('a')
+print('aa')
+print('aaa')
+print('-1111')
+print('faxinaar')
+print('goodbye cruel world')
