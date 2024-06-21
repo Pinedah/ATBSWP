ae336047bf8db4aea4a0935a2131e8185edde206
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-21 12:40:39 -0600

    Entering Expressions from Chapter 1
    Basic arithmetic stuff

---
+++ Part 1 - Python Programming Basics/Chapter 1 - Python Basics/1_EnteringExpressions.py
@@ -0,0 +1,33 @@
+
+# Entering Expressions 
+print(f"2 + 2: {2 + 2}\n")
+
+# Math Operators from Highest to Lowest Precedence
+
+print(f"------------------\nOperator: **\nExponent \n2 ** 3 = {2**3}")
+
+print(f"------------------\nOperator: %\nModulus/remainder \n22 % 8 = {22 % 8}")
+
+print(f"------------------\nOperator: //\nInteger division \n22 // 8 = {22 // 8}")
+
+print(f"------------------\nOperator: /\nDivision \n22 / 8 = {22 / 8}")
+
+print(f"------------------\nOperator: *\nMultiplication \n11 * 8 = {11 * 8}")
+
+print(f"------------------\nOperator: -\nSubtraction \n22 - 8 = {22 - 8}")
+
+print(f"------------------\nOperator: /\nAddition \n22 + 8 = {22 + 8}")
+
+# ////////////////////////////////////////////////
+
+# Order of operations matters
+
+print("________________________________")
+print(f"2 + 3 * 6 = {2 + 3 * 6}")
+print("---------")
+print(f"(2 + 3) * 6 = {(2 + 3) * 6}")
+
+# ////////////////////////////////////////////////
+
+# Python will display: SyntaxError if the instruction given is unclear
+
