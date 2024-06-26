6c08a10e1e90341ce59952e4bb6596f612fefd33
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-26 07:41:35 -0600

    Create Functions With Parameters from Chapter 3

---
+++ Part 1 - Python Programming Basics/Chapter 3 - Functions/1_functionsWithParameters.py
@@ -0,0 +1,8 @@
+
+def hello(name):
+    print('\nhello ' + name)
+
+hello('panke')
+# hello(11) # as long as the variable in the argument in of the function must be used like a string, the parameters passed must be strings
+
+hello(str(11)) # this could be a solution lmao
