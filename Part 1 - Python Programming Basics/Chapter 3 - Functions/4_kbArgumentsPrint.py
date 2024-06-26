e424ffb7f607371fe2f7ff590f3723d6046f22d9
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-26 11:18:46 -0600

    Create Keyboard Arguments and Print from Chapter 3
    (i liked this one)

---
+++ Part 1 - Python Programming Basics/Chapter 3 - Functions/4_kbArgumentsPrint.py
@@ -0,0 +1,25 @@
+
+#
+print()
+
+# output ->
+
+print('hello')
+print('World')
+
+# -> hello
+# -> world
+
+print('hello', end = '')
+print('World')
+
+# -> helloWorld
+
+
+print('cats', 'dogs', 'mice')
+# -> cats dogs mice
+
+print('cats', 'dogs', 'mice', sep = ',')
+# -> cats,dogs,mice
+
+print('panke', 'rol', 'dona', 'bolillo', sep = '---')
