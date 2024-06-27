0c7ac689c319236dac37c270af372c3b749a3d1e
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-27 08:08:27 -0600

    Create negative indexes (why?) from Chapter 4

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/1_negativeIndexes.py
@@ -0,0 +1,8 @@
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+print()
+print(spam[-1])
+# -> elephant
+print(spam[-3])
+# -> bat
+print( 'The ' + spam[-1] + ' is afraid of the ' + spam[int(-3.0)] + '.')
