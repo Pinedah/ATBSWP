7100e4ad0f1fc2e43e89f0fda3a72b1bba5b2d20
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-27 13:15:44 -0600

    Create list concatenation from Chapter 4
    wtf (again)

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/1_listConcatenation.py
@@ -0,0 +1,16 @@
+
+letters = ['A', 'B', 'C']
+numbers = [1, 2, 3]
+print()
+
+wtf = letters + numbers
+print(wtf)
+print()
+
+wtf *= 3
+print(wtf)
+print()
+
+wtf += ['xdddd', 33]
+print(wtf)
+print()
