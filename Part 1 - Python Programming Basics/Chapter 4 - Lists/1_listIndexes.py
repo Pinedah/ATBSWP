8add639981c10e848d0c7fb79f902d6b775362f8
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-27 07:53:45 -0600

    Create list indexes from Chapter 4

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/1_listIndexes.py
@@ -0,0 +1,14 @@
+
+spam = ['cat', 'bat', 'rat', 'dog']
+
+print()
+print(spam[0])
+# -> cat
+print(spam[1])
+# -> bat
+print(spam[2])
+# -> rat 
+print(spam[3])
+# -> dog
+
+print('The ' + spam[2] + ' ate the ' + spam[0] + '.')
