aeab8f61ce6d9a6729d51e32674da88a85f00741
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-27 07:39:58 -0600

    Create first file from Chapter 4
    Also, create the folder for its programs

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/0_listDataType.py
@@ -0,0 +1,17 @@
+
+numbers = [1, 2, 3]
+# -> [1, 2, 3]
+print(numbers)
+
+animals = ['cat', 'bat', 'rat', 'dog']
+# -> ['cat', 'bat', 'rat', 'dog']
+print(animals)
+
+wtf = ['hello', 3,1415, True, None, 42]
+# -> ['hello', 3,1415, True, None, 42]
+print(wtf)
+
+spam = ['cat', 'bat', 'rat', 'dog']
+print(spam)
+# -> ['cat', 'bat', 'rat', 'dog']
+
