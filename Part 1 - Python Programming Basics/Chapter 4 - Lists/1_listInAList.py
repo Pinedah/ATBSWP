e4d2a6ce72acf1189ec104d233d6137697fefff0
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-27 08:02:42 -0600

    Create List in a list from Chapter 4
    This is like an double array, but in list we can combine data types (like in a structure?)

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/1_listInAList.py
@@ -0,0 +1,10 @@
+
+spam = [['cat', 'bat'], [10, 20, 30, 40, 50]]
+
+# If you only use one index, the program will print the full list value at that index.
+print(spam[0])
+
+# The first index dictates which list value to use, and the second indicates the value within the list value.
+
+print(spam[0][1]) # bat
+print(spam[1][4]) # 50
