c9803bef6301af0a3645f9c42018bfa6e443b7e5
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-27 12:56:31 -0600

    Create Changing List Values from Chapter 4
    Assign values as usual

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/1_changingListValues.py
@@ -0,0 +1,16 @@
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+#       0       1       2       3
+
+
+print('\n' + str(spam))
+spam[1] = 'pankesito rex'
+
+print('\n' + str(spam))
+
+spam[2] = spam[1]
+print('\n' + str(spam))
+
+spam[-1] = 12345
+print('\n' + str(spam))
+
