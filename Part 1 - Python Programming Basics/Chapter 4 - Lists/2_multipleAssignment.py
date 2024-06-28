25b28b2f2a2596523df9860543bc9ee0ac272c42
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-27 19:49:54 -0600

    Create Multiple Assignment from Chapter 4
    this is a pro tip for assign multiple variables with the values in a list in only one line of code!
    this one cool

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/2_multipleAssignment.py
@@ -0,0 +1,13 @@
+print()
+cat = ['fat', 'orange', 'loud']
+
+size = cat[0]
+color = cat[1]
+disposition = cat[2]
+
+print(size, color, disposition)
+
+dog = ['so fat', 'black', 'lazy']
+size, color, disposition = dog
+
+print(size, color, disposition)
