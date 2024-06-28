9cf2d3400db00fac8ef7ffe2fd032aa310d3f3db
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-27 19:33:13 -0600

    Create In and Not In from Chapter 4
    evaluates to Boolean values

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/2_inNotIn.py
@@ -0,0 +1,12 @@
+
+print()
+# in and not in operators
+# can evaluate whether a value is or is not in a list
+
+print('panke' in ['dona', 'rol', 'bolillo'])
+print('panke' not in ['dona', 'rol', 'bolillo'])
+
+panes = ['panke', 'dona', 'telera']
+
+print('panke' in panes)
+print('amortiguador' not in panes)
