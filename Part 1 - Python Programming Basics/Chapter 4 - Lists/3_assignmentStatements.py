8789e20eb5d720b9cd5adace1b771b4eb6acd221
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-27 19:59:16 -0600

    Create Assignment Statements from Chapter 4

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/3_assignmentStatements.py
@@ -0,0 +1,24 @@
+
+"""
+    spam += 1
+    spam -= 1
+    spam *= 1
+    spam /= 1
+    spam //= 1
+    spam %= 1
+"""
+
+num = 11
+print(num)
+num *= 2
+print(num)
+num //= 3           # also works with //= for the int division
+print(num)
+
+
+spam = ['pan', 11, 'box']
+print(spam)
+spam += ['grr']
+print(spam)
+spam *= 3
+print(spam)
