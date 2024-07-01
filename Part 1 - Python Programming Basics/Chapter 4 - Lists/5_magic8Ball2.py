30c5ff95b157a858d14b799ff3b2916bb7605306
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-01 09:36:54 -0600

    Magic 8 Ball v2 from Chapter 4
    This program works exactly the same as the v1, but it doesn't include the elif cases and is way too shorter

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/5_magic8Ball2.py
@@ -0,0 +1,17 @@
+
+# 
+
+import random
+
+messages = ['it is certain',
+            'it is decidedly so',
+            'reply hazy try again',
+            'ask again later',
+            'concentrate and ask again',
+            'my reply is no',
+            'outlook not so good',
+            'very doubtful'
+            ]
+
+print(messages[random.randint(0, len(messages) - 1)])
+
