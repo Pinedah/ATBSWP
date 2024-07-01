55b75c2e8da7bb3e061a68a51a2eb685680b7997
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-06-30 22:33:19 -0600

    Append and Insert Methods from Chapter 4
    append inserts the value in the end of the list
    insert receives the index where you want to insert the value

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/4_append-insertMethods.py
@@ -0,0 +1,20 @@
+
+# append() 
+# insert(index_in_the_list, value)
+
+spam =  ['cat', 'dog', 'bat']
+
+print(spam)
+
+spam.append('pig')
+print(spam)
+
+spam.insert(1, 'wtf')
+print(spam)
+
+# also can append or insert a number, cause its py
+spam.append(11)
+print(spam)
+
+spam.insert(0, 33) # can u repeat that number?
+print(spam)
