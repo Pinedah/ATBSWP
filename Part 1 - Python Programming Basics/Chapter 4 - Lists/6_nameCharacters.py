7e8dcac97e3c6febf3748109cb5aeee2c16c3ccc
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-01 09:51:04 -0600

    String as List from Chapter 4

---
+++ Part 1 - Python Programming Basics/Chapter 4 - Lists/6_nameCharacters.py
@@ -0,0 +1,16 @@
+
+# using a string as a list
+
+name = 'fatima' 
+print(name)
+
+print(name[0])
+print(name[-2])
+print(name[0:4])
+
+print('fati' in name)
+print('faxi' in name)
+print('f' not in name)
+
+for i in name:
+    print('♡ * ♡ ' + i + ' ♡ * ♡')
