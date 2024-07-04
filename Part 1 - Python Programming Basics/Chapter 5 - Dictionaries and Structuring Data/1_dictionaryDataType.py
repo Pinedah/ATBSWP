abe4bf233343c34145992647dc3c90bfb89c1dc7
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-04 15:37:03 -0600

    Create Chapter 5 and firs program

---
+++ Part 1 - Python Programming Basics/Chapter 5 - Dictionaries and Structuring Data/1_dictionaryDataType.py
@@ -0,0 +1,9 @@
+
+#  In code, a dictionary is typed with braces, {}
+
+myCat = {'size': 'fat', 'color': 'orange', 'disposition': 'loud'}
+
+print('\n' + myCat['size'])
+# fat
+print('My cat has ' + myCat['color'] + ' fur')
+# My cat has orange fur
