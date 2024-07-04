167db0212db7e0cdba6cd6a4439210abdfc615c6
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-04 16:28:37 -0600

    Create dictionary order from Chapter 5
    dictionaries are equal even when their keys are not in the same order

---
+++ Part 1 - Python Programming Basics/Chapter 5 - Dictionaries and Structuring Data/2_dictionary-list.py
@@ -0,0 +1,9 @@
+
+
+spam = ['cats', 'dogs', 'moose']
+bacon = ['dogs', 'moose', 'cats']
+print(spam == bacon)
+
+eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+print(eggs == ham)
