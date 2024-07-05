f553ab4ce8a3da83b239da5d9ae68aa8b7ea740b
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-05 15:18:46 -0600

    Create Pretty Printing from Chapter 5
    import pprint
    this module will help to print dictionaries in a prettiest way (sorted, wtf)

---
+++ Part 1 - Python Programming Basics/Chapter 5 - Dictionaries and Structuring Data/2_prettyCharacterCount.py
@@ -0,0 +1,15 @@
+
+import pprint
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count) # print the dic sorted
+
+st = pprint.pformat(count) # pformat() will get the string value
+print(st) # will print the same as pprint.pprint()
