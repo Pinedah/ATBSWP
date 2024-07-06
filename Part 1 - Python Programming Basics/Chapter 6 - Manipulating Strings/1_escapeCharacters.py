7276ad3302f42d928ca301f39a4ee7f78f12ea7c
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-06 12:35:10 -0600

    Create Escape Characters from Chapter 6

---
+++ Part 1 - Python Programming Basics/Chapter 6 - Manipulating Strings/1_escapeCharacters.py
@@ -0,0 +1,11 @@
+
+"""
+    Escape character           Prints as
+    \'                         Single quote
+    \"                         Double quote
+    \t                         Tab
+    \n                         Newline (line break)
+    \\                         Backslash
+"""
+
+print("\n\"Hello there!\"\nHow are you?\nI\'m doing fine.\n\t\t\t\\\\...?")
