3365ba27b7ba0ad280c5789f831b6451cb165f7f
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-06 13:42:21 -0600

    Create Triple Quote string from Chapter 6

---
+++ Part 1 - Python Programming Basics/Chapter 6 - Manipulating Strings/1_catnapping.py
@@ -0,0 +1,9 @@
+
+# with triple quotes there is no need to use the \n or any escape character
+
+print('''Dear Fati,
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+      
+Sincerely,
+Panke.''')
+
