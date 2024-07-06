c07d39a29bc372e419a3dba1e3f20d3f685e8644
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-06 14:52:53 -0600

    Create 2_isXMethods.py

---
+++ Part 1 - Python Programming Basics/Chapter 6 - Manipulating Strings/2_isXMethods.py
@@ -0,0 +1,17 @@
+
+# 
+print()
+
+
+print(''''hello'.isalpha():\t\t\t''' + str('hello'.isalpha()))
+print(''''hello123'.isalpha():\t\t\t''' + str('hello123'.isalpha()))
+print(''''hello123'.isalnum():\t\t\t''' + str('hello123'.isalnum()))
+print(''''hello'.isalnum():\t\t\t''' + str('hello'.isalnum()))
+print(''''123'.isdecimal():\t\t\t''' + str('123'.isdecimal()))
+print(''''     '.isspace():\t\t\t''' + str('     '.isspace()))
+
+print(''''This Is Title Case'.istitle():\t\t''' + str('This Is Title Case'.istitle()))
+print(''''This Is Title Case 123'.istitle():\t''' + str('This Is Title Case 123'.istitle()))
+print(''''This Is not Title Case'.istitle():\t''' + str('This Is not Title Case'.istitle()))
+print(''''This Is NOT Title Case'.istitle():\t''' + str('This Is NOT Title Case'.istitle()))
+
