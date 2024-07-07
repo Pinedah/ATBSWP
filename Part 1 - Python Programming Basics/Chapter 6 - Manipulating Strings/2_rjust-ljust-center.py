d2285b9597f5935c46c260c42a64c385fd715e58
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-06 18:02:40 -0600

    Create rjust ljust center Methods from Chapter 6

---
+++ Part 1 - Python Programming Basics/Chapter 6 - Manipulating Strings/2_rjust-ljust-center.py
@@ -0,0 +1,15 @@
+
+print()
+
+print('Fati'.rjust(10))
+print('Fati'.rjust(20))
+print('Fatima Nava'.rjust(20))
+print('Faxi'.ljust(20))
+
+print()
+print('Faxinaar'.rjust(20, '*'))
+print('Faxinaar'.ljust(20, '*'))
+
+print()
+print('Faxinaar'.center(20, '-'))
+print('Faxinaar Nava'.center(20, '-'))
