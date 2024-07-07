6f07bca16f3d528d12b70b5db616050eab132c11
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-06 18:43:00 -0600

    Create pyperclip Module from Chapter 6
    amazing!

---
+++ Part 1 - Python Programming Basics/Chapter 6 - Manipulating Strings/2_pyperclip.py
@@ -0,0 +1,6 @@
+
+import pyperclip
+
+pyperclip.copy('this will appear in the clipboard!!!')
+
+print(pyperclip.paste()) # this will paste the text copied in the clipboard, if you got another string there, it will be displayed here!!!
