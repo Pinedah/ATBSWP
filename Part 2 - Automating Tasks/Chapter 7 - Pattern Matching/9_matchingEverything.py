1a1698ec1c06794496a94f02e30e5323c295c0db
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-09 22:14:48 -0600

    Update Matching Everything from Chapter 7

--- Part 2 - Automating Tasks/Chapter 7 - Pattern Matching/9_matchingEverything.py
+++ Part 2 - Automating Tasks/Chapter 7 - Pattern Matching/9_matchingEverything.py
@@ -8,3 +8,8 @@ nameRegex = re.compile(r'First Name: (.*) Last Name: (.*)')
 mo = nameRegex.search('First Name: Fatima Last Name: Nava')
 print(mo.group(1)) # Fatima
 print(mo.group(2)) # Nava
+
+mo2 = nameRegex.search('First Name: Whatever 33 Last Name: For Real, whatever lol : saymyname!!')
+print(mo2.group(1)) # Whatever 33
+print(mo2.group(2)) # For Real, whatever lol : saymyname!!
+
