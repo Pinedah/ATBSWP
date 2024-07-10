8f22fdb63e3379359dc920dd6a80891f3a781d84
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-09 22:54:07 -0600

    Create Case-Insensitive Matching from Chapter 7

---
+++ Part 2 - Automating Tasks/Chapter 7 - Pattern Matching/10_caseInsensitive.py
@@ -0,0 +1,17 @@
+#! python3
+# Case-Insensitive Matching
+
+import re
+
+robocop = re.compile(r'robocop', re.I)
+
+rc1 = robocop.search('RoboCop is part man, part machine, all cop.')
+print(rc1.group())
+
+rc2 = robocop.search('ROBOCOP protects the innocent.')
+print(rc2.group())
+
+rc3 = robocop.search('Al, why does your programming book talk about robocop so much?')
+print(rc3.group())
+
+
