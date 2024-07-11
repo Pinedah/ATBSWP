3f31b7e6851fda1d576beefc281df1eb2abe02a9
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-11 13:24:32 -0600

    Update 16_commasRegex.py

--- Part 2 - Automating Tasks/Chapter 7 - Pattern Matching/16_commasRegex.py
+++ Part 2 - Automating Tasks/Chapter 7 - Pattern Matching/16_commasRegex.py
@@ -3,7 +3,6 @@
 
 import re, pprint
 
-
 numRegex = re.compile(r'^\d{1,3}(,\d{3})*$') # when using start (^) and end ($) characters, search() matches the first and last char, it need to be passed just a single string
 
 test = ['42', '1,234', '6,232,123', '12,34,121', '1234', '123,456,789,123,', '11', '1,000', '11,000', '1,2,3,4', '111,111', '0', '341,231,312']
