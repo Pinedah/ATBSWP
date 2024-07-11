7d4833d0bdfd2df0be8d294091ef5ea299e47315
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-11 14:05:36 -0600

    Update 16_wordRegex.py

--- Part 2 - Automating Tasks/Chapter 7 - Pattern Matching/16_wordRegex.py
+++ Part 2 - Automating Tasks/Chapter 7 - Pattern Matching/16_wordRegex.py
@@ -2,7 +2,7 @@
 
 import re, pprint
 
-regex = re.compile(r'(Alice|Bob|Carol)\s(eats|pets|throws)\s(apples|cats|baseballs)\.', re.IGNORECASE)
+regex = re.compile(r'(Alice|Bob|Carol)\s(eats|pets|throws)\s(apples|cats|baseballs)(\.)', re.IGNORECASE)
 
 fa = regex.findall('''
 it must match the following:
@@ -10,8 +10,11 @@ it must match the following:
 •	'Bob pets cats.'
 •	'Carol throws baseballs.'
 •	'Alice throws Apples.'
+•	'Alice throws Apples.'
 •	'BOB EATS CATS.'
+•	'AlIcE tHroWS cATs.'
 but not the following:
+•	'AlIcE tHroWS cATs'
 •	'RoboCop eats apples.'
 •	'ALICE THROWS FOOTBALLS.'
 •	'Carol eats 7 cats.'
