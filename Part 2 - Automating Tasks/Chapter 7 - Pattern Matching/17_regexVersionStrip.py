fe078a1e07c005bb8e32e79133f419b429400679
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-11 15:21:59 -0600

    Create Regex Version of Strip from Chapter 7

---
+++ Part 2 - Automating Tasks/Chapter 7 - Pattern Matching/17_regexVersionStrip.py
@@ -0,0 +1,34 @@
+#! python3
+# Regex Version of Strip()
+# Write a function that takes a string and does the same thing as the strip() string method
+
+import re
+
+def regexStrip(string, otherA):
+
+    if string == '':
+        return ''
+
+    match otherA:
+        case '':
+            regex = re.compile(r'\S+')
+            fa = regex.findall(str(string))
+            if fa:
+                return ' '.join(fa)
+            else:
+                return ''
+            
+        case _:
+            regex = re.compile(f'[^{otherA}]')
+            fa = regex.findall(str(string))
+            if fa:
+                return ''.join(fa)
+            else:
+                return string
+
+    
+print(regexStrip('xxxxxxPankesito Rexxxxxxxxxx', 'x'))
+print(regexStrip('xxxxxxPankesito Rexxxxxxxxxx', ''))
+print(regexStrip('               Pankesito Rex         ', 'e'))
+print(regexStrip('       Fatima Nava Araujo     ', '') + '.')
+print(regexStrip('* * * Fati*****ma N*ava *Araujo******', '*'))
