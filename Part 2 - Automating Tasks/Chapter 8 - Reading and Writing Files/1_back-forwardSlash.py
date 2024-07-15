a8a05027cf042e992a6e792db8ede3f7d2b00f88
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-15 13:14:51 -0600

    Create Back and Forward Slash from Chapter 8
    the function os.path.join()  will add the correct format for every os

---
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/1_back-forwardSlash.py
@@ -0,0 +1,9 @@
+#!python3 
+# backslash for windows and forward slash for linux
+
+import os
+
+path = os.path.join('usr', 'bin', 'panke')
+print(path) # it will add automatically the \
+# usr\bin\panke
+
