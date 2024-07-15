d4fe705092ba065dd715e10e0cd119aa8b339a8b
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-15 14:39:34 -0600

    Create Dirname and Basename from Chapter 8

---
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/1_dirname-basename.py
@@ -0,0 +1,14 @@
+#! python3
+# 
+
+import os
+
+path = 'C:\\Windows\\System32\\calc.exe'
+#        dirname              || basename
+
+print(os.path.dirname(path)) # 
+print(os.path.basename(path)) #
+print(os.path.split(path)) # tuple with (dirname, basename)
+
+path_tuple = (os.path.dirname(path), os.path.basename(path))
+print(path_tuple) # exactly same tuple that with split
