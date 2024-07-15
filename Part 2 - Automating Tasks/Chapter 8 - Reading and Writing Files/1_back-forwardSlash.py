cd13ddcb7fa03ffc6f6425c7fdc82fc3752c3164
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-15 13:18:21 -0600

    Update 1_back-forwardSlash.py

--- Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/1_back-forwardSlash.py
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/1_back-forwardSlash.py
@@ -3,7 +3,13 @@
 
 import os
 
+print()
 path = os.path.join('usr', 'bin', 'panke')
 print(path) # it will add automatically the \
 # usr\bin\panke
 
+myFiles = ['accounst.txt', 'details.csv', 'invite.docx']
+
+print()
+for filename in myFiles:
+    print(os.path.join('C:\\Users\\panke', filename))
