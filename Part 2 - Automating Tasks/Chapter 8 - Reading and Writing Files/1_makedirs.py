6ec9db404ea5883b8a4dfec00735d67c26a02819
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-15 13:45:11 -0600

    Create makedirs Function and Dirs from Chapter 8

---
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/1_makedirs.py
@@ -0,0 +1,8 @@
+#!python3 
+# Creating New Folders with os.makedirs()
+
+import os
+
+os.makedirs('C:\\Users\\Dell Latitude\\Documents\\GitHub\\ATBSWP\\Part 2 - Automating Tasks\\Chapter 8 - Reading and Writing Files\\newDirectoy1\\newDirectory2')
+
+print('\nDirs created')
