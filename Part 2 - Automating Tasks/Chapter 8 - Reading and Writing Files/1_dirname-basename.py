8d700bc74449500c57cee60063b7a6e76129fafb
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-15 15:06:24 -0600

    Update 1_dirname-basename.py

--- Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/1_dirname-basename.py
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/1_dirname-basename.py
@@ -12,3 +12,6 @@ print(os.path.split(path)) # tuple with (dirname, basename)
 
 path_tuple = (os.path.dirname(path), os.path.basename(path))
 print(path_tuple) # exactly same tuple that with split
+
+print(path.split(os.path.sep))
+print(os.getcwd().split(os.path.sep))
