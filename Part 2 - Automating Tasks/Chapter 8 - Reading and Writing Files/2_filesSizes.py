2fd2df7f5e072bd813213daa8498be3d8be278e6
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-18 10:35:51 -0600

    Create File Sizes from Chapter 8

---
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/2_filesSizes.py
@@ -0,0 +1,12 @@
+#! python3 
+# Finding File Sizes and Folder Contents
+
+import os
+
+print()
+
+print()
+print(os.path.getsize('C:\\Users\\Dell Latitude\\Documents\\CECyT 3\\CECyT Calificaciones.xlsx'))
+
+print()
+print(os.path.getsize('C:\\Users\\Dell Latitude\\Desktop\\ESCOM\\mapaCurricularLCD2020.pdf'))
