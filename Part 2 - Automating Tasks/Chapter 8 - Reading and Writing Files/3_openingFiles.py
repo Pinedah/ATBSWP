c356b60b2d1cb054c48d01a17a001654664b100f
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-18 11:23:38 -0600

    Update 3_openingFiles.py

--- Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/3_openingFiles.py
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/3_openingFiles.py
@@ -2,4 +2,9 @@
 # Opening Files with the open() Function
 
 helloFile = open('C:\\Users\\Dell Latitude\\Documents\\GitHub\\ATBSWP\\Part 2 - Automating Tasks\\Chapter 8 - Reading and Writing Files\\3_files\\hi.txt')
+
+helloFile = open('C:\\Users\\Dell Latitude\\Documents\\GitHub\\ATBSWP\\Part 2 - Automating Tasks\\Chapter 8 - Reading and Writing Files\\3_files\\hi.txt', 'r') 
+# 'r' opens the file in READ mode
+
+
 # Open function returns a File object
