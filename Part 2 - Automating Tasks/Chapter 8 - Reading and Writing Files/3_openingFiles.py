3f65f9d439fef651bdb68111d040b11a76db7eeb
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-18 11:21:14 -0600

    Create Files Dir for 3 from Chapter 8

---
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/3_openingFiles.py
@@ -0,0 +1,5 @@
+#! python3
+# Opening Files with the open() Function
+
+helloFile = open('C:\\Users\\Dell Latitude\\Documents\\GitHub\\ATBSWP\\Part 2 - Automating Tasks\\Chapter 8 - Reading and Writing Files\\3_files\\hi.txt')
+# Open function returns a File object
