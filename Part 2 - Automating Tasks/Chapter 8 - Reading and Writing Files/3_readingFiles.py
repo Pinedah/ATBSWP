866190e5a6c9a0a22b2a989c45672df891e19b79
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-18 11:28:11 -0600

    Create Reading Files from Chapter 8

---
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/3_readingFiles.py
@@ -0,0 +1,9 @@
+#! python3
+# Reading the Contents of Files
+
+
+helloFile = open('C:\\Users\\Dell Latitude\\Documents\\GitHub\\ATBSWP\\Part 2 - Automating Tasks\\Chapter 8 - Reading and Writing Files\\3_files\\hi.txt')
+
+helloContent = helloFile.read()
+
+print(helloContent)
