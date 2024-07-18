80a484190f953f235a1f81e444b1c35df946e2ff
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-18 11:31:16 -0600

    Update reading and its file

--- Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/3_readingFiles.py
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/3_readingFiles.py
@@ -5,5 +5,9 @@
 helloFile = open('C:\\Users\\Dell Latitude\\Documents\\GitHub\\ATBSWP\\Part 2 - Automating Tasks\\Chapter 8 - Reading and Writing Files\\3_files\\hi.txt')
 
 helloContent = helloFile.read()
-
 print(helloContent)
+
+print()
+sonnetFile = open('3_files\\sonnet29.txt')
+sonnetContent = sonnetFile.readlines()
+print(sonnetContent)
