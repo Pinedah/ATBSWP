29149a19a42b102236ec5c1aacd16a44aa0c0835
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-18 19:12:52 -0600

    Create Import myCats from Chapter 8

---
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/5_Saving Variables pprint/5_sv-pprint2.py
@@ -0,0 +1,15 @@
+#! python3
+# Importing the new module called myCats
+
+import myCats
+
+print('\nThe module myCats imported: \n')
+print(myCats.cats)
+
+print()
+print(myCats.cats[0])
+print(myCats.cats[0]['name'])
+
+print()
+print(myCats.cats[1])
+print(myCats.cats[1]['desc'])
