fdb4185694b0956f8b82180f1f11101763457e87
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-21 22:37:59 -0600

    Update regexSearch.py

--- Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/10_regexSearch/regexSearch.py
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/10_regexSearch/regexSearch.py
@@ -1,9 +1,11 @@
 #! python3
 # Write a program that opens all .txt files in a folder and searches for any line that matches a user-supplied regular expression. The results should be printed to the screen. 
 
-
+import re
 
 # TODO: User enter the regex
+input = str(input())
+userRegex = re.compile(input)
 
 # TODO: Read LINES in every file in a folder
 
