bf5402a4a24b7d3eac2106dd814f09546e75de35
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-21 22:44:11 -0600

    Update regexSearch.py

--- Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/10_regexSearch/regexSearch.py
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/10_regexSearch/regexSearch.py
@@ -1,13 +1,22 @@
 #! python3
 # Write a program that opens all .txt files in a folder and searches for any line that matches a user-supplied regular expression. The results should be printed to the screen. 
 
-import re
+import re, os
 
 # TODO: User enter the regex
-input = str(input())
+input = str(input('Enter your regex: '))
 userRegex = re.compile(input)
 
+# TODO: Access to every file in a folder
+folder = 'C:\\Users\\Dell Latitude\\Documents\\GitHub\\ATBSWP\\Part 2 - Automating Tasks\\Chapter 8 - Reading and Writing Files\\10_regexSearch\\files'
+
+if os.path.exists(folder):
+    files = os.listdir(folder)
+    print(files)
+
 # TODO: Read LINES in every file in a folder
+
+
 
 # TODO: Look for the lines that matches with the regex
 
