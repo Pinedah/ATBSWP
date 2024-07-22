fbd99d753a1ab85428e5712604eff9776a1aaea4
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-21 22:27:49 -0600

    Create Regex Search project from Chapter 8

---
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/10_regexSearch/regexSearch.py
@@ -0,0 +1,12 @@
+#! python3
+# Write a program that opens all .txt files in a folder and searches for any line that matches a user-supplied regular expression. The results should be printed to the screen. 
+
+
+
+# TODO: User enter the regex
+
+# TODO: Read LINES in every file in a folder
+
+# TODO: Look for the lines that matches with the regex
+
+# TODO: Print the results
