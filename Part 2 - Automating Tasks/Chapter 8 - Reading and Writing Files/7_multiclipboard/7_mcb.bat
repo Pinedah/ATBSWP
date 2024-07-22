4986f218d0d1dd1fe48fd3ec27c15d7b66181a00
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-21 18:36:07 -0600

    Update 7_mcb.bat

--- Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/7_multiclipboard/7_mcb.bat
+++ Part 2 - Automating Tasks/Chapter 8 - Reading and Writing Files/7_multiclipboard/7_mcb.bat
@@ -1,1 +1,1 @@
-@pyw.exe "C:\Users\Dell Latitude\Documents\GitHub\ATBSWP\Part 2 - Automating Tasks\Chapter 8 - Reading and Writing Files\7_mcp.pyw" %*
+@pyw.exe "C:\Users\Dell Latitude\Documents\GitHub\ATBSWP\Part 2 - Automating Tasks\Chapter 8 - Reading and Writing Files\7_multiclipboard\7_mcb.pyw" %*
