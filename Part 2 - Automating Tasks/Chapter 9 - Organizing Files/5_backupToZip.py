b9c1c1a510ca9d1faf8b102109d81fce41177723
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-24 21:19:20 -0600

    Update 5_backupToZip.py

--- Part 2 - Automating Tasks/Chapter 9 - Organizing Files/5_backupToZip.py
+++ Part 2 - Automating Tasks/Chapter 9 - Organizing Files/5_backupToZip.py
@@ -20,9 +20,21 @@ def backupToZip(folder):
         # Create the ZIP file
         print(f'Creating {zipFilename}...')
         backupZip = zipfile.ZipFile(zipFilename, 'w')
-        
-        # TODO: Walk the entire folder tree and compress the files in each folder 
 
+        # Walk the entire folder tree and compress the files in each folder 
+        for foldername, subfolders, filenames in os.walk(folder):
+            print(f'Adding diles in {foldername}')
+            
+            # Add the current folder to the ZIP file.
+            backupZip.write(foldername)
+
+            # Add all the files in this folder to the ZIP file.
+            for filename in filenames:
+                newBase = os.path.basename(folder) + '_'
+                if filename.startswith(newBase) and filename.endswith('.zip'):
+                    continue # don't backup the backup ZIP files
+                backupZip.write(os.path.join(foldername, filename))
+        backupZip.close()
     print('Done.')
 
-backupToZip('C:\\')
+backupToZip('2_delicious')
