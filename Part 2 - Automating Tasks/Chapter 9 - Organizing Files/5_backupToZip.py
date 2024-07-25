1a524c704e1af68d6f69cf3f61709640d8ebb8b5
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-24 21:04:00 -0600

    Update 5_backupToZip.py

--- Part 2 - Automating Tasks/Chapter 9 - Organizing Files/5_backupToZip.py
+++ Part 2 - Automating Tasks/Chapter 9 - Organizing Files/5_backupToZip.py
@@ -5,3 +5,23 @@ import zipfile, os
 
 def backupToZip(folder):
     # Backup the entire contents of "folder" into a ZIP file.
+
+    folder = os.path.abspath(folder) # make sure folder is absolute
+
+    # Figure ot the filename this code should use based on what files already exists
+
+    number = 1
+    
+    while True:
+        zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
+        if not os.path.exists(zipFilename):
+            break
+        number += 1
+
+    # TODO: Create the ZIP file
+
+    # TODO: Walk the entire folder tree and compress the files in each folder 
+
+    print('Done.')
+
+backupToZip('C:\\')
