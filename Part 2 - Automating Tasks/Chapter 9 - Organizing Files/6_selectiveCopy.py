99376c636f0cf93fde895b1f4b1b45d0531142fb
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-25 20:56:16 -0600

    Update 6_selectiveCopy.py

--- Part 2 - Automating Tasks/Chapter 9 - Organizing Files/6_selectiveCopy.py
+++ Part 2 - Automating Tasks/Chapter 9 - Organizing Files/6_selectiveCopy.py
@@ -8,21 +8,25 @@ import os, pprint, shutil, re
 
 dir = ''
 
-extensionsRegex = re.compile(r'(.)(txt|py|c)')
+extensionsRegex = re.compile(r'(.*?)((.)(txt|py|c))$')
 
 # Create new folder
-newDir = ''
+newDir = 'newD'
 if not os.path.exists(newDir):
     os.makedirs(newDir)
 
+toMove = []
 # Walk into the directory
 for folderName, subfolders, filenames in os.walk(dir):
-    #print(f'The current folder is {folderName}')
-    for subfolder in subfolders:
-        #print(f'SUBFOLDER OF {folderName}: {subfolder}')
+    #for subfolder in subfolders:
     for filename in filenames:
-        #print(f'FILE INSIDE {folderName}: {filename}')
+        if extensionsRegex.match():
+            #toMove.append(filename)
+            print(f'{filename} appended')
 
 
 # Move into the new foler
 
+for file in toMove:
+    #shutil.copy(file, newDir)
+    print(f'{file} copied to {newDir}')
