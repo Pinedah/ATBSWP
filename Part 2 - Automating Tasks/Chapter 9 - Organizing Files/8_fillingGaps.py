759d239c355ff4ef296093eed5e081e153fab438
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-25 23:14:50 -0600

    Update 8_fillingGaps.py

--- Part 2 - Automating Tasks/Chapter 9 - Organizing Files/8_fillingGaps.py
+++ Part 2 - Automating Tasks/Chapter 9 - Organizing Files/8_fillingGaps.py
@@ -12,13 +12,10 @@ for file in os.listdir(dir):
     if prefixRegex.match(file):
         files[file] = prefixRegex.match(file).group(3)
 
-#pprint.pprint(files)
-
 min = int(list(files.values())[0])
 for filenumber in files.values():
     if int(filenumber) < min:
         min = int(filenumber)
-#print('min:', min)
 
 sortedFiles = {}
 
@@ -26,23 +23,17 @@ filenumbers = []
 for filenumber in files.values():
     filenumbers.append(int(filenumber))
 filenumbers.sort()
-#print(filenumbers)
 
 newValues = {}
 for number in filenumbers:
     newValues[number] = min
     min += 1
-#pprint.pprint(newValues)
 
 newF = []
 for k, v in files.items():
     if int(v) in list(newValues.keys()):
-        # files[k] = str(newValues[int(v)]).zfill(3)
         files[k] = str(k).replace(v, str(newValues[int(v)]).zfill(3))
-        # newF.append(str(k).replace(v, str(newValues[int(v)]).zfill(3)))
 
-#pprint.pprint(files)
-# print(newF)
 
 nFiles = []
 
@@ -51,16 +42,3 @@ nFiles = []
 for file in files:
     shutil.move(os.path.join(dir, file), os.path.join(dir, files[file]))
     print(f'{os.path.join(dir, file)} renamed as: {files[file]}')
-"""
-
-for v in files.values():
-    if int(v) == 
-
-
-for i in range(len(filenumbers)):
-    # print(i)
-    filenumbers[i] = i + 1
-print(filenumbers)
-
-
-"""
