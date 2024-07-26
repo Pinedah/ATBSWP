4f22c0eb73e0a2d9e92551dc345a9ccd300a8739
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-25 20:48:39 -0600

    Update 6_selectiveCopy.py

--- Part 2 - Automating Tasks/Chapter 9 - Organizing Files/6_selectiveCopy.py
+++ Part 2 - Automating Tasks/Chapter 9 - Organizing Files/6_selectiveCopy.py
@@ -5,3 +5,24 @@ Write a program that walks through a folder tree and searches for files with a c
 """
 
 import os, pprint, shutil, re
+
+dir = ''
+
+extensionsRegex = re.compile(r'(.)(txt|py|c)')
+
+# Create new folder
+newDir = ''
+if not os.path.exists(newDir):
+    os.makedirs(newDir)
+
+# Walk into the directory
+for folderName, subfolders, filenames in os.walk(dir):
+    #print(f'The current folder is {folderName}')
+    for subfolder in subfolders:
+        #print(f'SUBFOLDER OF {folderName}: {subfolder}')
+    for filename in filenames:
+        #print(f'FILE INSIDE {folderName}: {filename}')
+
+
+# Move into the new foler
+
