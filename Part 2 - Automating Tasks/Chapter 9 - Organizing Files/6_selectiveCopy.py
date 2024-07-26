ec26df429c4e9397f3b66d952ee9a724e065e7fc
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-25 21:15:34 -0600

    Finished

--- Part 2 - Automating Tasks/Chapter 9 - Organizing Files/6_selectiveCopy.py
+++ Part 2 - Automating Tasks/Chapter 9 - Organizing Files/6_selectiveCopy.py
@@ -3,30 +3,34 @@
 """
 Write a program that walks through a folder tree and searches for files with a certain file extension (such as .pdf or .jpg). Copy these files from whatever location they are in to a new folder.
 """
+print()
 
 import os, pprint, shutil, re
 
-dir = ''
-
-extensionsRegex = re.compile(r'(.*?)((.)(txt|py|c))$')
+dir = 'selectiveCopy' # directory where to search
+extensionsRegex = re.compile(r'(.*?)((.)(txt|py|c))$') # put the searched extensions here
 
 # Create new folder
-newDir = 'newD'
+newDir = 'justSelectives'
 if not os.path.exists(newDir):
     os.makedirs(newDir)
 
 toMove = []
-# Walk into the directory
-for folderName, subfolders, filenames in os.walk(dir):
-    #for subfolder in subfolders:
-    for filename in filenames:
-        if extensionsRegex.match():
-            #toMove.append(filename)
-            print(f'{filename} appended')
 
+# Walk into the directory
+pprint.pprint(list(os.walk(dir)))
+print()
+
+for folderName, subfolders, filenames in os.walk(dir):
+    for filename in filenames:
+        if extensionsRegex.match(filename):
+            toMove.append(os.path.join(folderName, filename))
+            print(f'{filename} -', end='')
+print()
 
 # Move into the new foler
-
+pprint.pprint(toMove)
+print()
 for file in toMove:
-    #shutil.copy(file, newDir)
+    shutil.copy(file, newDir)
     print(f'{file} copied to {newDir}')
