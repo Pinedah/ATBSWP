9888cd6e6db229b3906b79679d85d11788bba8fd
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-25 21:38:29 -0600

    Update 7_deletingUnneeded.py

--- Part 2 - Automating Tasks/Chapter 9 - Organizing Files/7_deletingUnneeded.py
+++ Part 2 - Automating Tasks/Chapter 9 - Organizing Files/7_deletingUnneeded.py
@@ -1,25 +1,22 @@
 #! python3
 # Write a program that walks through a folder tree and searches for excep tionally large files or folders—say, ones that have a file size of more than 100MB. 
 
-import os, pprint, send2trash
+import os, send2trash
 
 maxSize = 5000000
 dir = "C:\\Users\\Dell Latitude\\Documents\\LIBROS"
 dir2 = "C:\\Users\\Dell Latitude\\Documents\\CECyT 3"
 
-
-print()
-#pprint.pprint(list(os.walk(dir)))
-
-
 # Walk the directory
 for folderName, subfolders, filenames in os.walk(dir2):
+
     for filename in filenames:
         file = os.path.join(folderName, filename)
+        
         # Check file size 
-        if os.path.getsize(os.path.join(folderName, filename)) > maxSize:
-            print(f'{filename} is too big, it will be deleted ({os.path.getsize(os.path.join(folderName, filename))})')
+        if os.path.getsize(file) > maxSize:
+            print(f'{filename} is too big, it will be deleted ({os.path.getsize(file)})')
 
             # Send to trash the files that are biggest than the limit
-            # send2trash.send2trash(os.path.join(folderName, filename))
+            # send2trash.send2trash(os.path.join(folderName, filename)) DO NOT UNCOMMENT THIS SHIT ! (im using my highschool evidence folder for the example)
                                   
