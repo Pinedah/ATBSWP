c1bc8cb4b8752033a652bf4312aaa89528f29e55
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-29 12:51:20 -0600

    Create Disabling logging from Chapter 10

---
+++ Part 2 - Automating Tasks/Chapter 10 - Debugging/4_disablingLogging.py
@@ -0,0 +1,15 @@
+#! python3
+# Disabling Logging using logging.disable()
+
+import logging
+
+logging.basicConfig(level = logging.INFO, format = ' %(asctime)s - %(levelname)s - %(message)s')
+
+logging.critical('Critical error! Critical error!')
+logging.critical('Critical error! Critical error!')
+
+logging.disable(logging.CRITICAL)
+
+logging.critical('Critical error! Critical error!')
+
+logging.error('Error! Error!')
