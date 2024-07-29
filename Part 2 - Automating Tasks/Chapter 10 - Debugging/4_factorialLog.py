3fb2f52588528c41584b4bfebca3d6047a6eada6
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-29 10:24:34 -0600

    Create factorialLog with Bug from chapter 10

---
+++ Part 2 - Automating Tasks/Chapter 10 - Debugging/4_factorialLog.py
@@ -0,0 +1,20 @@
+#! python3
+# 
+
+import logging
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s -  %(levelname)s -   %(message)s')
+# necessary to use the %()s format
+
+logging.debug('Start of program')
+
+def factorial(n):
+    logging.debug(f'Start tof factorial {n}')
+    total = 1
+    for i in range(n + 1):
+        total *= i
+        logging.debug(f'i is {i}, total is {total}')
+    logging.debug(f'End of factorial {n}')
+    return total
+
+print(factorial(5))
+logging.debug('End of program')
