17d56dba5270f448002c26193fb1b5d33ede0d65
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-28 22:55:30 -0600

    Commented Assertion file from Chapter 10

--- Part 2 - Automating Tasks/Chapter 10 - Debugging/3_assertions.py
+++ Part 2 - Automating Tasks/Chapter 10 - Debugging/3_assertions.py
@@ -1,7 +1,21 @@
 #! python3
 # Assertions 
 
+# the variable is defined as 'open'
 podBayDoorStatus = 'open'
+
+# assert define the conditions as 'open', and the AssertionError as 'The pod...'
 assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
+# the var is open, so its TRUE, and the program can continue
+
+# the variable now is overwritten as 'I'm sorry ...'
 podBayDoorStatus = "I'm sorry, Dave. I'm afraid I can't do that."
-assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
+
+
+
+# Assert evaluates again 'open', as long as its false, now displays the AssertionError
+assert podBayDoorStatus == 'open', "AssertionError changed to -> PANKE"
+# change the error message for PANKE
+
+
+print("The program does't get here")
