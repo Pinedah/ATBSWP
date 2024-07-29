d5d8886b5b3ac80c3d54996c0eac648f2b1ebee8
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-29 14:01:52 -0600

    Create 5_debugging.py

---
+++ Part 2 - Automating Tasks/Chapter 10 - Debugging/5_debugging.py
@@ -0,0 +1,15 @@
+#! python3
+# Debugging a Number Adding Program
+
+print('Enter the first number to add: ')
+first = input()
+
+print('Enter the second number to add: ')
+second = input()
+
+print('Enter the third number to add: ')
+third = input()
+
+print(f'The sum is {first + second + third}')
+
+
