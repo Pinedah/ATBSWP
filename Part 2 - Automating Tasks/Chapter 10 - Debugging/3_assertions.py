7ccb056fe8d273494245f05fb89def60180ab032
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-28 22:46:49 -0600

    Create Assertions file1 from Chapter 10

---
+++ Part 2 - Automating Tasks/Chapter 10 - Debugging/3_assertions.py
@@ -0,0 +1,7 @@
+#! python3
+# Assertions 
+
+podBayDoorStatus = 'open'
+assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
+podBayDoorStatus = "I'm sorry, Dave. I'm afraid I can't do that."
+assert podBayDoorStatus == 'open', 'The pod bay doors need to be "open".'
