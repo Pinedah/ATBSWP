5a08a498d36b104e9be9e877fb2b9e40058f9003
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-29 10:29:25 -0600

    Update 4_factorialLog.py
    correct the bug with logging help

--- Part 2 - Automating Tasks/Chapter 10 - Debugging/4_factorialLog.py
+++ Part 2 - Automating Tasks/Chapter 10 - Debugging/4_factorialLog.py
@@ -10,7 +10,7 @@ logging.debug('Start of program')
 def factorial(n):
     logging.debug(f'Start tof factorial {n}')
     total = 1
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         total *= i
         logging.debug(f'i is {i}, total is {total}')
     logging.debug(f'End of factorial {n}')
