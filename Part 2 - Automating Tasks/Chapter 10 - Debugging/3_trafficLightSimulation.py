bdd6af6cff810ee7ffd77f2b2479affc4d9c704d
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-28 23:41:45 -0600

    Create traffic lights simul from Chapter 10

---
+++ Part 2 - Automating Tasks/Chapter 10 - Debugging/3_trafficLightSimulation.py
@@ -0,0 +1,19 @@
+#! python3 
+# Using an Assertion in a Traffic Light Simulation 
+
+market_2nd = {'ns': 'green', 'ew': 'red'}
+mission_16th = {'ns': 'red', 'ew': 'green'}
+
+def switchLights(stoplight):
+    for key in stoplight.keys():
+        if stoplight[key] == 'green':
+            stoplight[key] = 'yellow'
+        elif stoplight[key] == 'yellow':
+            stoplight[key] = 'red'
+        elif stoplight[key] == 'red':
+            stoplight[key] = 'green'
+
+print(market_2nd)
+
+switchLights(market_2nd)
+print(market_2nd)
