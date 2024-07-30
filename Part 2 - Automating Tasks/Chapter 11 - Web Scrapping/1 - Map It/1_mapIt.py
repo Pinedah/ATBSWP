47e7528c9a3ae7cdcda9bf258e910c9aeaf5ad86
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-30 13:53:10 -0600

    Update 1_mapIt.py

--- Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/1 - Map It/1_mapIt.py
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/1 - Map It/1_mapIt.py
@@ -1,10 +1,22 @@
 #! python3
 # 1_mapIt.py - Launches a map in the browser using an address from the command line or clipboard
 
-import webbrowser, sys, logging
+import webbrowser, sys, logging, pyperclip
+
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s -  %(levelname)s -   %(message)s')
+
+logging.info("Starting program... ... ...")
+logging.debug(sys.argv)
 
 if len(sys.argv) > 1:
     # Get the address from command line
     address = ' '.join(sys.argv[1:])
+    logging.info(address)
+else:
+    # Get the adress from the clipboard
+    address = pyperclip.paste()
+    logging.info(address)
 
-# TODO : Get the adress from the clipboard
+webbrowser.open('https://www.google.com/maps/place/' + address)
+
+logging.info("Ending program...\n")
