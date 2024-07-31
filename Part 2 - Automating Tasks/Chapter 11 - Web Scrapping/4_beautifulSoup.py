11f84f83c37701c090d0e7a29909bc8b267ad691
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-31 10:36:22 -0600

    Update 4_beautifulSoup.py

--- Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/4_beautifulSoup.py
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/4_beautifulSoup.py
@@ -5,13 +5,14 @@ import requests, bs4
 
 res = requests.get('http://nostarch.com')
 
-res.raise_for_status()
-noStarchSoup = bs4.BeautifulSoup(res.text, features="html.parser")
-print(type(noStarchSoup))
+res.raise_for_status() # veifies everything's ok
+noStarchSoup = bs4.BeautifulSoup(res.text, features="html.parser") # create the bs4 object 
+# features = "html..." specifies the parser library that BeautifulSoup should use to parse the HTML content.
+print(type(noStarchSoup)) # print the type
 
 print('-------------------------------------------\n\n')
 
-exampleFile = open('example.html')
+exampleFile = open('example.html') # also can be opened an local file
 
 exampleSoup = bs4.BeautifulSoup(exampleFile, features="html.parser")
 print(type(exampleSoup))
