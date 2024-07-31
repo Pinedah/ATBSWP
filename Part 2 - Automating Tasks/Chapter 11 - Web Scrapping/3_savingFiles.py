90689991313f13422813304554706d604e3ac2e2
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-07-30 23:39:45 -0600

    Download the file from a webpage

--- Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/3_savingFiles.py
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/3_savingFiles.py
@@ -1,3 +1,13 @@
 #! python3
 # Saving Downloaded Files to the Hard Drive
 
+import requests
+res = requests.get('http://www.gutenberg.org/cache/epub/1112/pg1112.txt')
+
+res.raise_for_status()
+playFile = open('RomeoAndJuliet.txt', 'wb')
+
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+
+playFile.close()
