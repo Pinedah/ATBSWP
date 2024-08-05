526a69975886a20e3c4558d257dd8edd70eb1228
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-08-05 10:05:56 -0600

    Update 4_bs4.py

--- Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/4_bs4.py
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/4_bs4.py
@@ -13,18 +13,24 @@ print()
 
 print("Elems: ", end='')
 print(elems)
+
 print("type: ", end='')
 print(type(elems))
+
 print("len: ", end='')
 print(len(elems))
+
 print("type(elems[0]): ", end='')
 print(type(elems[0]))
+
 print("elems[0].getText(): ", end='')
 print(elems[0].getText())
 print("elems[1].getText(): ", end='')
 print(elems[1].getText())
+
 print("str(elems[0]): ", end='')
 print(str(elems[0]))
+
 print("elems[0].attrs: ", end='')
 print(elems[0].attrs)
 
