3e651213d94d97eb0ced56522f7413faf1cf690f
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-08-05 12:44:24 -0600

    Update lucky.py
    add comments

--- Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/5 - Lucky/lucky.py
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/5 - Lucky/lucky.py
@@ -13,27 +13,19 @@ logging.info('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
-#logging.info(res.text)
 
-# TODO: Retrieve top search result links.
+# Retrieve top search result links.
 soup = bs4.BeautifulSoup(res.text, features='html.parser')
 
-# TODO: Open a browser tab for each result.
+# Open a browser tab for each result.
+searchResults = soup.select('.egMi0') # class = "egMi0" -> search divs that contain search links
+logging.info(pprint.pformat(searchResults))
 
-# linkElems = soup.select('.r a')
-linkElems = soup.select('.egMi0') # class = "yuRUbf"
+linkSoup = bs4.BeautifulSoup(str(searchResults), features='html.parser')
+linkElems = linkSoup.select('a') # class = "yuRUbf" -> just links from the divs
 logging.info(pprint.pformat(linkElems))
 
-aSoup = bs4.BeautifulSoup(str(linkElems), features='html.parser')
-
-linkElems2 = aSoup.select('a') # class = "yuRUbf"
-
-#logging.info(pprint.pformat(linkElems))
-logging.info(pprint.pformat(linkElems2))
-# logging.info(pprint.pformat(linkElems2[0].get('href')))
-
-
-numOpen = min(5, len(linkElems))
+numOpen = min(5, len(linkElems)) # define the number of tabs to be open
 for i in range(numOpen):
-    webbrowser.open('http://google.com' + linkElems2[i].get('href'))
-    logging.info('http://google.com' + linkElems2[i].get('href'))
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
+    logging.info('http://google.com' + linkElems[i].get('href'))
