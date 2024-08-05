5ecae7d84ed8ba0cc56eb326ac60cf0bab5721f2
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-08-05 10:36:32 -0600

    Update lucky.py

---
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/5 - Lucky/lucky.py
@@ -0,0 +1,14 @@
+#! python3
+# lucky.py - Opens several Google search results.
+
+import requests, sys, webbrowser, bs4
+
+print('Googling...') # display text while downloading the Google page
+
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+# TODO: Retrieve top search result links.
+
+# TODO: Open a browser tab for each result.
+
