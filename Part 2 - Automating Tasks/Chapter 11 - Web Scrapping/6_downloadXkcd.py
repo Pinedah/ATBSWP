c49772cd512e059c899d851fdc00bca9e5bad224
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-08-05 14:50:51 -0600

    Delete thousands of images lol

--- Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/6_downloadXkcd.py
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/6_downloadXkcd.py
@@ -4,13 +4,13 @@
 import requests, os, bs4, logging
 
 logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s -  %(levelname)s -   %(message)s')
-# logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 
 url = "https://xkcd.com/"
 os.makedirs('xkcd', exist_ok=True)
 
-while not url.endswith('#'):
-    # TODO: Download the page
+for i in range(5):
+    # Download the page
     print(f"Downloading the page... {url}")
     res = requests.get(url)
     res.raise_for_status()
@@ -18,7 +18,7 @@ while not url.endswith('#'):
     logging.info(res.text)
     soup = bs4.BeautifulSoup(res.text, features='html.parser')
 
-    # TODO: Find the URL of the comic image.
+    # Find the URL of the comic image.
     comicElem = soup.select('#comic img')
 
     logging.info(comicElem)
@@ -30,20 +30,23 @@ while not url.endswith('#'):
         print("Could not find comic image.")
     else:
         comicUrl = 'https://xkcd.com' + comicElem[0].get('src')
-        # TODO: Download the image
+        # Download the image
         print(f"\nDownloading the image... {comicUrl}")
         
         res = requests.get(comicUrl)
         res.raise_for_status()
 
-        # TODO: Save the image to ./xkcd
+        # Save the image to ./xkcd
         imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+
+        """
         for chunk in res.iter_content(100000):
             imageFile.write(chunk)
         imageFile.close()
+        """
 
-    # TODO: Get the Prev butto's url
+    # Get the Prev button's url
     prevLink = soup.select('a[rel="prev"]')[0]
     url = 'http://xkcd.com' + prevLink.get('href')
 
-print("Done.")
+print("\nDONE.")
