ad35230b68ce323ee6ce4fb4fe3a8d9db04a659d
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-08-05 10:27:18 -0600

    Create getMethod

--- Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/4_getMethod.py
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/4_getMethod.py
@@ -8,13 +8,16 @@ soup = bs4.BeautifulSoup(open('example.html'), features='html.parser')
 print()
 print(soup.select('span')) # list
 spanElement = soup.select('span')[0]
-spanElement2 = soup.select('span')[1]
 
 print(str(spanElement))
 print(spanElement.get('id'))
-print(spanElement2.get('class'))
 print(spanElement.get('some_nonexistent_addr') == None)
 print(spanElement.attrs)
+print()
+
+spanElement2 = soup.select('span')[1]
+print(spanElement2.attrs)
+print(spanElement2.get('class'))
 
 print()
 
