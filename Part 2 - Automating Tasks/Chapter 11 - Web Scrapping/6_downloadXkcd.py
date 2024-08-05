836db76db609cb2f6cdfef55464e343470ff5acd
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-08-05 14:45:24 -0600

    Run project

--- Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/6_downloadXkcd.py
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/6_downloadXkcd.py
@@ -4,7 +4,7 @@
 import requests, os, bs4, logging
 
 logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s -  %(levelname)s -   %(message)s')
-logging.disable(logging.CRITICAL)
+# logging.disable(logging.CRITICAL)
 
 url = "https://xkcd.com/"
 os.makedirs('xkcd', exist_ok=True)
@@ -16,16 +16,22 @@ while not url.endswith('#'):
     res.raise_for_status()
 
     logging.info(res.text)
-    soup = bs4.BeautifulSoup(res.text)
+    soup = bs4.BeautifulSoup(res.text, features='html.parser')
 
     # TODO: Find the URL of the comic image.
     comicElem = soup.select('#comic img')
+
+    logging.info(comicElem)
+    logging.info(len(comicElem))
+    logging.info(comicElem[0].attrs)
+    logging.info(comicElem[0].get('src'))
+
     if comicElem == []:
         print("Could not find comic image.")
     else:
-        comicUrl = comicElem[0].get('src')
+        comicUrl = 'https://xkcd.com' + comicElem[0].get('src')
         # TODO: Download the image
-        print(f"Downloading the image... {comicUrl}")
+        print(f"\nDownloading the image... {comicUrl}")
         
         res = requests.get(comicUrl)
         res.raise_for_status()
