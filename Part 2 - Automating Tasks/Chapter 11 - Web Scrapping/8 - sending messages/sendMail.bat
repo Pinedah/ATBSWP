d5f6a50fed0137454423c7877f765ae08adacd95
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-08-07 12:20:07 -0600

    Create bat file

---
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/8 - sending messages/sendMail.bat
@@ -0,0 +1,2 @@
+@py.exe "C:\Users\Dell Latitude\Documents\GitHub\ATBSWP\Part 2 - Automating Tasks\Chapter 11 - Web Scrapping\8 - sending messages\sendingMail.py" %*
+@pause
--- Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/8 - sending messages/sendingMail.py
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/8 - sending messages/sendingMail.py
@@ -1,8 +1,29 @@
 #! python3
 # Sending an email from the WIN + R
 
+import sys, logging
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+# logging config 
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s -  %(levelname)s -   %(message)s')
+# logging.disable(logging.CRITICAL)
 
 # User Credentials
 userEmail = 'papanacho.nacho@gmail.com'
 userPass = 'Papanacho12345'
 
+browser = webdriver.Chrome()
+try:
+    browser.get('http://gmail.com')
+    
+    emailElem = browser.find_element(By.TAG_NAME, 'input')
+    emailElem.send_keys(userEmail)
+    
+    emailButton = browser.find_element(By.TAG_NAME, 'button')
+    emailButton.click()
+
+except NoSuchElementException:
+    print("\nNope.")
