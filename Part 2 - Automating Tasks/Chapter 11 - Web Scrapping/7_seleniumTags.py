0d34448fc9a74c65832b17202f1651d4f444dffa
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-08-07 10:21:38 -0600

    Create Get Tags with selenium

---
+++ Part 2 - Automating Tasks/Chapter 11 - Web Scrapping/7_seleniumTags.py
@@ -0,0 +1,26 @@
+#! python3
+# Finding elements on the page
+
+import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s -  %(levelname)s -   %(message)s')
+#logging.disable(logging.CRITICAL)
+
+browser = webdriver.Chrome()
+browser.get('http://inventwithpython.com')
+
+try:
+    elem = browser.find_element(By.CLASS_NAME, 'card-img-top')
+    print(f"\n\nFound <{elem.tag_name}> element with that class name!\n\n")
+    print(f"Found <{elem.get_attribute('alt')}> attribute\n\n!")
+except NoSuchElementException:
+    print("Was not able to find an element with that class name.")
+
+# Wait for user input before closing the browser
+input("Press Enter to close the browser...")
+
+# Close the browser
+browser.quit()
